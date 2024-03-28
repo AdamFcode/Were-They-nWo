19bf2cfec3961dd0dd958365087555dc323efcee
AdamFcode <adamfeehely@gmail.com>
2024-03-28 20:31:00 +0000

    add some questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const question = [
+    { 
+        img: "assets/images/buff-bagwell.webp",
+        p: "",
+        select: ["nWo for life!", "Jabroni!"],
+        answer: "nWo for life!",
+    },
+    {
+        img: "assets/images/chris-jericho.webp",
+        p: "",
+        select: ["nWo for life!", "Jabroni!"],
+        answer: "nWo for life!",
+    },
+
+]
